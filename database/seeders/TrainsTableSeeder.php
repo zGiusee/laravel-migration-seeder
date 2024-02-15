ca654bc41783a848f9bede628193ef10e944c513
zGiusee <giusebelforte@gmail.com>
2024-02-15 15:55:29 +0100

    [UPDATE] Popolata la tabella del database

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,6 +16,22 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $newTrain = new Train();
+        for ($i = 0; $i < 30; $i++) {
+            $newTrain = new Train();
+
+            $newTrain->company = $faker->words(2, true);
+            $newTrain->departure_station = $faker->words(5, true);
+            $newTrain->arrival_station = $faker->words(5, true);
+            $newTrain->departure_date = $faker->date();
+            $newTrain->arrival_date = $faker->date();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code = $faker->numberBetween(10000, 99999);
+            $newTrain->carriages_number = $faker->numberBetween(5, 20);
+            $newTrain->in_time = $faker->boolean();
+            $newTrain->deleted = $faker->boolean();
+
+            $newTrain->save();
+        };
     }
 }
