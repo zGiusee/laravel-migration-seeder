afc3e2b5d0c83fa7bfb8cb385d9438157da1b7df
zGiusee <giusebelforte@gmail.com>
2024-02-15 15:30:59 +0100

    [UPDATE] created seeder and controller

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $trains = Train::all();
+        return view('home', compact('trains'));
     }
 };
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $newTrain = new Train();
+    }
+}
